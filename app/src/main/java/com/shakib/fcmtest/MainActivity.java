16c83567b2228e1e011b2ab33e8de56f08e55639
ShaQib <shakib.possesed@gmail.com>
2019-06-26 21:38:44 +0600

    updated

--- app/src/main/java/com/shakib/fcmtest/MainActivity.java
+++ app/src/main/java/com/shakib/fcmtest/MainActivity.java
@@ -8,31 +8,41 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CHANNEL_ID = "FCM_ID";
     private static final String CHANNEL_NAME = "FCM";
     private static final String CHANNEL_DESC = "FCM desc";
+
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
 
@@ -41,6 +51,20 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager mNotificationManager = getSystemService(NotificationManager.class);
             mNotificationManager.createNotificationChannel(mChannel);
         }
+
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.login_sign_up);
+        progressBar = findViewById(R.id.progress);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                createUser();
+
+            }
+        });
 
         //getting FCM token
         FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
@@ -53,6 +77,90 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //getting FCM token
+
+    }
+
+    private void createUser() {
+
+        final String email = etEmail.getText().toString().trim();
+        final String password = etPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+
+            etEmail.setError("Email required");
+            etEmail.requestFocus();
+            return;
+
+        }
+
+        if (password.isEmpty()){
+
+            etPassword.setError("Password required");
+            etPassword.requestFocus();
+            return;
+
+        }
+
+        if (password.length() < 6){
+
+            etPassword.setError("Password should be at least 6 character long");
+            etPassword.requestFocus();
+            return;
+
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                    startProfileActivity();
+
+                } else {
+
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException){
+
+                        userLogin(email, password);
+
+                    } else{
+
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    private void userLogin(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                    startProfileActivity();
+
+                } else {
+
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+
+    private void startProfileActivity() {
+
+        Intent intent = new Intent(this, ProfileActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |  Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
 
     }
 
